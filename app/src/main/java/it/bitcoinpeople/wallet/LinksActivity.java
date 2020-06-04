0265825d199ef21fc4086e875b295133775edca4
Alekos Filini <alekos.filini@gmail.com>
2020-06-04 14:36:49 +0200

    Remove bar in LinksActivity

--- app/src/main/java/it/bitcoinpeople/wallet/LinksActivity.java
+++ app/src/main/java/it/bitcoinpeople/wallet/LinksActivity.java
@@ -14,7 +14,6 @@ public class LinksActivity extends GaActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_links);
-        setTitleBackTransparent();
 
         TextView t = findViewById(R.id.links_text);
         t.setText(Html.fromHtml("Acquista sul sito <a href=\"https://bitcoinpeople.it\">bitcoinpeople.it</a><br>Metti Mi Piace alla <a href=\"https://www.facebook.com/bitcoinpeople.it/\">Pagina Facebook</a><br>Contattaci a <a href=\"mailto:info@bitcoinpeople.it\">info@bitcoinpeople.it</a>"));
